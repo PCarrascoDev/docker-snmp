b42c70fcd4c2084cfcf9ca9b3ea3a4361f206436
root <pedro.carrascoc@mail.udp.cl>
2020-09-29 17:13:14 -0300

    Added test queries

--- server/server-script.sh
+++ server/server-script.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-while true; do sleep 15 ; snmpget -v2c -c public 192.168.0.8 sysDescr.0; done
+while true; do 
+    sleep 300 ;
+    echo "System description: " && snmpget -v2c -c public 192.168.0.8 sysDescr.0; 
+done &
+
+while true; do
+    sleep 60 ;
+    echo "System status: " && snmpstatus -v2c -c public 192.168.0.8;
+done &
+
+while true; do
+    sleep 3600 ;
+    echo "SNMP Walk " && snmpwalk -v2c -c public 192.168.0.8 > snmpwalkout.txt ;
+done 
