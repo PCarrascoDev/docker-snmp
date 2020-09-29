70f6c5b0667fb7d25559fb2b9ea6b32d30dcfa1b
root <pedro.carrascoc@mail.udp.cl>
2020-09-29 17:52:31 -0300

    Added Arg verification

--- server/server-script.sh
+++ server/server-script.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-while true; do 
-    sleep 300 ;
-    echo "System description: " && snmpget -v2c -c public 192.168.0.8 sysDescr.0; 
-done &
+# echo Your container args are: "$@"
+# echo "$1"
+# echo "$2"
 
-while true; do
-    sleep 60 ;
-    echo "System status: " && snmpstatus -v2c -c public 192.168.0.8;
-done &
+if [ $# -ne 0 ] 
+then
+    echo "Starting Admin SNMP daemon for agent on $1"
+    echo "System description: " && snmpget -v2c -c public $1 sysDescr.0;
+    while true; do 
+        sleep 300 ;
+        echo "System description: " && snmpget -v2c -c public $1 sysDescr.0; 
+    done &
 
-while true; do
-    sleep 3600 ;
-    echo "SNMP Walk " && snmpwalk -v2c -c public 192.168.0.8 > snmpwalkout.txt ;
-done 
+    while true; do
+        sleep 60 ;
+        echo "System status: " && snmpstatus -v2c -c public $1;
+    done &
+
+    while true; do
+        sleep 3600 ;
+        echo "SNMP Walk " && snmpwalk -v2c -c public $1 > snmpwalkout.txt ;
+    done 
+else
+    echo "Please specify IP Address of agent machine"
+fi
