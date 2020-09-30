118050108300c85ae4309cfdd920a671c74049d1
Pedro <pedro.carrascoc@mail.udp.cl>
2020-09-30 13:25:58 -0300

    Fixed ip comprobation

--- server/server-script.sh
+++ server/server-script.sh
@@ -3,38 +3,46 @@
 #Test for arguments
 if [ $# -ne 0 ] 
 then
-    echo "Starting Admin SNMP daemon for agent on $1"
-    echo "System description: " && snmpget -v2c -c public $1 sysDescr.0;
+    if expr "$1" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null || [ "$1" = "localhost" ]; then
 
-    #Request sysDescription every 5min
-    while true; do 
-        sleep 300 ;
-        echo "System description: " && snmpget -v2c -c public $1 sysDescr.0; 
-    done &
+        echo "Starting Admin SNMP daemon for agent on $1"
+        echo "System description: " && snmpget -v2c -c public $1 sysDescr.0;
 
-    #Request sysStatus every minute
-    while true; do
-        sleep 60 ;
-        echo "System status: " && snmpstatus -v2c -c public $1;
-    done &
+        #Request sysDescription every 5min
+        while true; do 
+            sleep 300 ;
+            echo "System description: " && snmpget -v2c -c public $1 sysDescr.0; 
+        done &
 
-    #Do a full snmp walk every hour
-    while true; do
-        sleep 3600 ;
-        echo "SNMP Walk " && snmpwalk -v2c -c public $1 > snmpwalkout.txt ;
-    done &
+        #Request sysStatus every minute
+        while true; do
+            sleep 60 ;
+            echo "System status: " && snmpstatus -v2c -c public $1;
+        done &
 
-    #Request device network status every 10sec
-    while true; do
-        sleep  10 ;
-        echo "Network status " && snmpnetstat -v2c -c public $1 ;
-    done &
+        #Do a full snmp walk every hour
+        while true; do
+            sleep 3600 ;
+            echo "SNMP Walk " && snmpwalk -v2c -c public $1 > snmpwalkout.txt ;
+        done &
 
-    #Update MIB's every 2 hours
-    while true; do
-        sleep  7200 ;
-        echo "Updating MIB's... " && download-mibs ;
-    done
+        #Request device network status every 10sec
+        while true; do
+            sleep  10 ;
+            echo "Network status " && snmpnetstat -v2c -c public $1 ;
+        done &
+
+        #Update MIB's every 2 hours
+        while true; do
+            sleep  7200 ;
+            echo "Updating MIB's... " && download-mibs ;
+        done
+
+    else
+    echo "Wrong agent address"
+
+    fi
+
 else
     echo "Please specify IP Address of agent machine"
 fi
