6ebad478aba8bb0630f9f36441d2fc5490e2b47e
root <pedro.carrascoc@mail.udp.cl>
2020-10-01 16:23:53 -0300

    Added Uptime and CPU load indicators

--- server/server-script.sh
+++ server/server-script.sh
@@ -8,35 +8,47 @@ then
         echo "Starting Admin SNMP daemon for agent on $1"
         echo "System description: " && snmpget -v2c -c public $1 sysDescr.0;
 
-        #Request sysDescription every 5min
+        #Request sysDescription
         while true; do 
-            sleep 300 ;
+            sleep 30 ;
             echo "System description: " && snmpget -v2c -c public $1 sysDescr.0; 
         done &
 
-        #Request sysStatus every minute
+        #Request sysStatus
         while true; do
-            sleep 60 ;
+            sleep 20 ;
             echo "System status: " && snmpstatus -v2c -c public $1;
         done &
 
-        #Do a full snmp walk every hour
+        #Do a full snmp
         while true; do
-            sleep 3600 ;
+            sleep 50 ;
             echo "SNMP Walk " && snmpwalk -v2c -c public $1 > snmpwalkout.txt ;
         done &
 
-        #Request device network status every 10sec
+        #Request device network status
         while true; do
             sleep  10 ;
             echo "Network status " && snmpnetstat -v2c -c public $1 ;
         done &
 
-        #Update MIB's every 2 hours
+        #Update MIB's
         while true; do
-            sleep  7200 ;
+            sleep  90 ;
             echo "Updating MIB's... " && download-mibs ;
-        done
+        done &
+
+        #Uptime
+        while true; do
+            sleep  40 ;
+            echo "Uptime: " && snmpget -v2c -c public $1 hrSystemNumUsers.0 ;
+        done  &
+
+        #CPU Load
+        while true; do
+            sleep  60 ;
+            echo "CPU Load: " && snmpwalk -v2c -c public $1 hrProcessorLoad.* ;
+        done 
 
     else
     echo "Wrong agent address"
