8300bd33304be8c9e68be689a7f797737db9e158
Pedro Carrasco <pedro.carrascoc@mail.udp.cl>
2020-10-22 16:32:54 -0300

    Fix remove_snmp_data.py

--- polymorph/functions/remove_snmp_data.py
+++ polymorph/functions/remove_snmp_data.py
@@ -1,7 +1,7 @@
 def remove_snmp_data(packet):
     try:
         if packet['UDP']['dstport'] == 161 or packet['UDP']['srcport'] == 161:
-            packet['SNMP']['data'] = None
+            packet['SNMP']['data'] = "0x0"
             print("Removed data from packet")
             return packet
     except:
