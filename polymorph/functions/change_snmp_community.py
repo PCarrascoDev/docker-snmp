b66068918a606f8395504e024a1b270c148984eb
Pedro Carrasco <pedro.carrascoc@mail.udp.cl>
2020-10-22 01:17:16 -0300

    Modified change_snmp_community.py

--- polymorph/functions/change_snmp_community.py
+++ polymorph/functions/change_snmp_community.py
@@ -2,8 +2,9 @@ def change_snmp_community(packet):
 
     try:
         if packet['UDP']['srcport'] == 161:
-            packet['SNMP']['community'] = None
-            print("Success")
+            new_comm = None
+            packet['SNMP']['community'] = new_comm
+            print("Changed SNMP community to: " str(new_comm))
             return packet
     except:
         print("Error")
