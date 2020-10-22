e5038324e3884e2dbbe7d4467bd21d58397edd7d
Pedro Carrasco <pedro.carrascoc@mail.udp.cl>
2020-10-22 16:44:58 -0300

    Added function files to polymorph docker
    Fix change_snmp_community.py

--- polymorph/functions/change_snmp_community.py
+++ polymorph/functions/change_snmp_community.py
@@ -2,7 +2,7 @@ def change_snmp_community(packet):
 
     try:
         if packet['UDP']['srcport'] == 161:
-            new_comm = None
+            new_comm = "None"
             packet['SNMP']['community'] = new_comm
             print("Changed SNMP community to: " + str(new_comm))
             return packet
